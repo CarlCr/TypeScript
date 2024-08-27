6c80eca341f13d5b298f25860af444a53c56ce4d
CarlCr <dev@carlos.ao>
2024-08-27 19:13:02 +0100

    feat:a dd using interface

---
+++ 2 - Custom Types/04-UsingInterfaces.ts
@@ -0,0 +1,29 @@
+interface InventoryItem{
+  name: string,
+  price: number,
+}
+interface Product extends InventoryItem{
+  color?: string,
+}
+interface Service extends InventoryItem{
+  startTime: Date,
+  endTime: Date,
+}
+
+const tshirt : Product = {
+  name: 'T-Shirt Design B',
+  price: 12
+}
+const photoShoot : Service = {
+  startTime: new Date('May 9, 2009 11:00:00'),
+  endTime: new Date('May 9, 2009 12:00:00'),
+  name: 'Kelly Wedding Photo Shoot',
+  price: 450
+}
+
+function purchaseItem(item: InventoryItem) : InventoryItem{
+  console.log(item.price)
+  return item
+}
+
+purchaseItem(photoShoot)
